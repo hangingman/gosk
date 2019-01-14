01d004e1957dbb84037217203b35249a2f98dfba
hiroyuki nagata <newserver002@gmail.com>
2019-01-14 22:06:09 +0900

    Add reflection using code

--- eval/eval_stmt_test.go
+++ eval/eval_stmt_test.go
@@ -1,30 +1,28 @@
 package eval
 
 import (
+	"fmt"
 	"github.com/hangingman/gosk/ast"
 	"github.com/hangingman/gosk/object"
-	// "github.com/hangingman/gosk/eval"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
 
 func TestEvalStatements(t *testing.T) {
-	// bin1 := Binary{Value: []byte{1, 2, 3}}
-	// bin2 := Binary{Value: []byte{4, 5, 6}}
-	// barray := BinaryArray{Value: []Binary{bin1, bin2}}
-	// assert.Equal(t, barray.Inspect(), "010203,040506")
-
 	d1 := &ast.DummyStatement{}
 	d2 := &ast.DummyStatement{}
 	d3 := &ast.DummyStatement{}
 	dArr := []ast.Statement{d1, d2, d3}
 
-	objArr, ok := evalStatements(dArr).(*object.ObjectArray)
+	evaluated := evalStatements(dArr)
+	assert.Equal(t, "*object.ObjectArray", reflect.TypeOf(evaluated).String())
+	objArr, ok := evaluated.(*object.ObjectArray)
+
 	assert.True(t, ok)
-	assert.Equal(t, len(*objArr), 3)
+	assert.Equal(t, 3, len(*objArr))
 	for _, obj := range *objArr {
+		assert.Equal(t, "*object.Binary", reflect.TypeOf(obj).String())
 		fmt.Printf("%s: %s\n", reflect.TypeOf(obj), obj.Inspect())
 		bin, _ := obj.(*object.Binary)
 		fmt.Printf("%s\n", bin.Inspect())
