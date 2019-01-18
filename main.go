dfe97ec9d92c10ae8bb6a578511f3b34fabf0ec6
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2019-01-18 10:49:38 +0900

    Add version string

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/hangingman/gosk/repl"
 	"os"
 	"os/user"
 )
 
+const Version = "1.0.0 beta"
+
 func main() {
+	var (
+		version = flag.Bool("v", false, "")
+	)
+	// -hオプション用文言
+	// 	flag.Usage = func() {
+	// 		fmt.Fprintf(os.Stderr, `
+	// Usage of %s:
+	//    %s [OPTIONS] ARGS...
+	// Options\n`, os.Args[0],os.Args[0])
+	// 		flag.PrintDefaults()
+	// 	}
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("gosk %s\n", Version)
+		fmt.Printf("%s", `Copyright (C) 2019 Hiroyuki Nagata.
+ライセンス GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Thank you osask project !`)
+		os.Exit(0)
+	}
+
+	// 引数が無ければREPLモードへ移行
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
