e881a290be48d422e212593b3fe62779d1e1d15d
hiroyuki nagata <newserver002@gmail.com>
2019-02-06 23:14:47 +0900

    Add labelManagement class that hold binary object reference

--- eval/eval_labels.go
+++ eval/eval_labels.go
@@ -2,20 +2,34 @@ package eval
 
 import (
 	"fmt"
+	"github.com/hangingman/gosk/object"
 	"log"
 )
 
 type LabelManagement struct {
+	labelBinaryRefMap map[string]*object.Binary // バイナリへの参照を記録し、後で更新する
+	labelBytesMap     map[string]int
+	opcode            map[string][]byte
 }
 
-func (l *LabelManagement) AddLabelCallback(ident string) {
-	log.Println(fmt.Sprintf("info: add label %s !!", ident))
+func (l *LabelManagement) AddLabelCallback(opcode []byte, ident string, bin *object.Binary, from int) {
+	log.Println(fmt.Sprintf("info: add label %s from %d !!", ident, from))
+	l.opcode[ident] = opcode
+	l.labelBinaryRefMap[ident] = bin
+	l.labelBytesMap[ident] = from
 }
 
 func (l *LabelManagement) RemoveLabelCallback(ident string) {
 	log.Println(fmt.Sprintf("info: remove label %s !!", ident))
 }
 
-func (l *LabelManagement) Emit(ident string) {
-	log.Println(fmt.Sprintf("info: emit label %s !!", ident))
+func (l *LabelManagement) Emit(ident string, to int) {
+	log.Println(fmt.Sprintf("info: emit label %s to %d !!", ident, to))
+	opcode, opcodeOk := l.opcode[ident]
+	bin, binOk := l.labelBinaryRefMap[ident]
+	from, fromOk := l.labelBytesMap[ident]
+	if opcodeOk && binOk && fromOk {
+		bin.Value = append(bin.Value, opcode...)
+		bin.Value = append(bin.Value, int2Byte(to-from)...)
+	}
 }
