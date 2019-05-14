a9f8fd93c74eefb110dc9712cd646fed5fa2a9df
hiroyuki nagata <newserver002@gmail.com>
2019-05-15 05:19:44 +0900

    Add testdata 03_day_harib00e_ipl.nas

--- eval/03_day_eval_test.go
+++ eval/03_day_eval_test.go
@@ -7,13 +7,15 @@ import (
 )
 
 func TestDumpHarib(t *testing.T) {
-	input := getAsmSource("03_day_harib00c_ipl.nas")
+	input := getAsmSource("03_day_harib00e_ipl.nas")
 	testAsmSourceOnlyDump(t, input, []string{""})
 
 	// 実際のテスト
 	t.Run("harib00a", testHarib00a)
 	t.Run("harib00b", testHarib00b)
 	t.Run("harib00c", testHarib00c)
+	t.Run("harib00d", testHarib00d)
+	t.Run("harib00e", testHarib00e)
 }
 
 // TestHelloOS3 naskソース３日目(harib00a)のテスト
@@ -89,6 +91,64 @@ func testHarib00c(t *testing.T) {
 000000a0  00 74 f1 b4 0e bb 0f 00  cd 10 eb ee 0a 0a 6c 6f  |.t............lo|
 000000b0  61 64 20 65 72 72 6f 72  0a 00 00 00 00 00 00 00  |ad error........|
 000000c0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+*
+000001f0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 55 aa  |..............U.|
+`
+
+	testAsmSource(t, input, strings.Split(answer, "\n"))
+}
+
+// TestHelloOS3 naskソース３日目(harib00d)のテスト
+func testHarib00d(t *testing.T) {
+	time.Sleep(1 * time.Second)
+	input := getAsmSource("03_day_harib00d_ipl.nas")
+
+	// wine nask.exe ipl.nas ipl.obj
+	// hexdump -C ipl.obj > ipl.hex
+	// ... generate .hex file little endian
+	answer := `00000000  eb 4e 90 48 41 52 49 42  4f 54 45 00 02 01 01 00  |.N.HARIBOTE.....|
+00000010  02 e0 00 40 0b f0 09 00  12 00 02 00 00 00 00 00  |...@............|
+00000020  40 0b 00 00 00 00 29 ff  ff ff ff 48 41 52 49 42  |@.....)....HARIB|
+00000030  4f 54 45 4f 53 20 46 41  54 31 32 20 20 20 00 00  |OTEOS FAT12   ..|
+00000040  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000050  b8 00 00 8e d0 bc 00 7c  8e d8 b8 20 08 8e c0 b5  |.......|... ....|
+00000060  00 b6 00 b1 02 be 00 00  b4 02 b0 01 bb 00 00 b2  |................|
+00000070  00 cd 13 73 10 83 c6 01  83 fe 05 73 2e b4 00 b2  |...s.......s....|
+00000080  00 cd 13 eb e3 8c c0 05  20 00 8e c0 80 c1 01 80  |........ .......|
+00000090  f9 12 76 d1 b1 01 80 c6  01 80 fe 02 72 c7 b6 00  |..v.........r...|
+000000a0  80 c5 01 80 fd 0a 72 bd  f4 eb fd be c0 7c 8a 04  |......r......|..|
+000000b0  83 c6 01 3c 00 74 f1 b4  0e bb 0f 00 cd 10 eb ee  |...<.t..........|
+000000c0  0a 0a 6c 6f 61 64 20 65  72 72 6f 72 0a 00 00 00  |..load error....|
+000000d0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+*
+000001f0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 55 aa  |..............U.|
+`
+
+	testAsmSource(t, input, strings.Split(answer, "\n"))
+}
+
+// TestHelloOS3 naskソース３日目(harib00e)のテスト
+func testHarib00e(t *testing.T) {
+	time.Sleep(1 * time.Second)
+	input := getAsmSource("03_day_harib00e_ipl.nas")
+
+	// wine nask.exe ipl.nas ipl.obj
+	// hexdump -C ipl.obj > ipl.hex
+	// ... generate .hex file little endian
+	answer := `00000000  eb 4e 90 48 41 52 49 42  4f 54 45 00 02 01 01 00  |.N.HARIBOTE.....|
+00000010  02 e0 00 40 0b f0 09 00  12 00 02 00 00 00 00 00  |...@............|
+00000020  40 0b 00 00 00 00 29 ff  ff ff ff 48 41 52 49 42  |@.....)....HARIB|
+00000030  4f 54 45 4f 53 20 46 41  54 31 32 20 20 20 00 00  |OTEOS FAT12   ..|
+00000040  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000050  b8 00 00 8e d0 bc 00 7c  8e d8 b8 20 08 8e c0 b5  |.......|... ....|
+00000060  00 b6 00 b1 02 be 00 00  b4 02 b0 01 bb 00 00 b2  |................|
+00000070  00 cd 13 73 10 83 c6 01  83 fe 05 73 2e b4 00 b2  |...s.......s....|
+00000080  00 cd 13 eb e3 8c c0 05  20 00 8e c0 80 c1 01 80  |........ .......|
+00000090  f9 12 76 d1 b1 01 80 c6  01 80 fe 02 72 c7 b6 00  |..v.........r...|
+000000a0  80 c5 01 80 fd 0a 72 bd  f4 eb fd be c0 7c 8a 04  |......r......|..|
+000000b0  83 c6 01 3c 00 74 f1 b4  0e bb 0f 00 cd 10 eb ee  |...<.t..........|
+000000c0  0a 0a 6c 6f 61 64 20 65  72 72 6f 72 0a 00 00 00  |..load error....|
+000000d0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
 *
 000001f0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 55 aa  |..............U.|
 `
